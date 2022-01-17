7ea8f5d4ff4b258a3185c948150cb3d2faa77f25
ErikHendrikson <erikhendrikson@hotmail.com>
2022-01-17 14:04:49 +0100

    add top of header navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,32 @@
 import React from "react";
 import "./Navigation.scss";
+import logo from "../images/logo.svg";
+import search from "../images/search.svg";
+import cart from "../images/cart.svg";
 
 const Navigation = () => {
   return (
     <header className="c-navigation">
-      <input type="checkbox" id="toggle" />
-      <label className="c-navigation__label" htmlFor="toggle">
-        <div className="c-navigation__hamburger">
-          <div className="c-navigation__hamburger__center"></div>
-        </div>
-      </label>
-      <nav></nav>
+
+      <div className="c-navigation__left">
+        <input type="checkbox" id="toggle" />
+        <label className="c-navigation__label" htmlFor="toggle">
+          <div className="c-navigation__hamburger">
+            <div className="c-navigation__hamburger__center" />
+          </div>
+        </label>
+
+
+          <img className="c-navigation__logo" src={logo} alt="logo" />
+
+
+      </div>
+
+      <div className="c-navigation__right">
+        <img className="c-navigation__image" src={search} alt="search" />
+        <img className="c-navigation__image" src={cart} alt="cart" />
+      </div>
+
     </header>
   );
 };
